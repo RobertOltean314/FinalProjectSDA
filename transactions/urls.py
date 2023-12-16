9a2db8184a1c1bd5349e1efabb5330fa523c8e11
Robert Oltean <olteanrobertadrian314@gmail.com>
2023-12-16 12:59:40 +0200

    Mici modificari

--- transactions/urls.py
+++ transactions/urls.py
@@ -4,6 +4,6 @@ from transactions import views
 urlpatterns = [
     path('create_transaction/', views.TransactionCreateView.as_view(), name='create-transaction'),
     path('list_of_transactions/', views.TransactionListView.as_view(), name='list-of-transactions'),
-    path('update_student/<int:pk>/', views.TransactionUpdateView.as_view(), name='update-student'),
-    path('delete_student/<int:pk>/', views.TransactionDeleteView.as_view(), name='delete-student'),
+    path('update_student/<int:pk>/', views.TransactionUpdateView.as_view(), name='update-transaction'),
+    path('delete_student/<int:pk>/', views.TransactionDeleteView.as_view(), name='delete-transaction'),
 ]
