853fb0151c793030645721ab7d4f4199a98e72df
Robert Oltean <olteanrobertadrian314@gmail.com>
2023-12-16 11:31:30 +0200

    Passowrd reset prin email

--- FinalProject/urls.py
+++ FinalProject/urls.py
@@ -26,11 +26,21 @@ import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
     path('register/', users_views.register, name='register'),
     path('account/', account_views.account, name='account'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+         name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
     path('logout/', users_views.logout_view, name='logout'),
+    path('', include('home.urls')),
 
 ]
 
