50c089d65cce57ef309d6ce78a1b8217b02fd25e
constantinus345 <96290754+constantinus345@users.noreply.github.com>
2023-12-17 11:59:18 +0200

    forms date modificat

--- transactions/forms.py
+++ transactions/forms.py
@@ -10,38 +10,16 @@ class CategoryForm(forms.ModelForm):
         fields = ['category_name']
 
 
+
 class TransactionForm(forms.ModelForm):
     category = forms.ModelChoiceField(queryset=Category.objects.all(), required=False)
     display_date = forms.DateField(initial=datetime.now().date(), widget=forms.DateInput(attrs={'type': 'date'}))
 
-    class TransactionForm(forms.ModelForm):
-        category = forms.ModelChoiceField(queryset=Category.objects.all(), required=False)
-        display_date = forms.DateField(initial=datetime.now().date(), widget=forms.DateInput(attrs={'type': 'date'}))
-
-        class Meta:
-            model = Transaction
-            fields = ['date', 'description', 'amount', 'amount_type', 'category']
-            widgets = {
-                'date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
-                'description': forms.Textarea(
-                    attrs={'class': 'form-control', 'placeholder': 'Please enter your description', 'rows': 3}),
-                'amount': forms.NumberInput(
-                    attrs={'class': 'form-control', 'placeholder': 'Please enter the amount of money you want'}),
-                'amount_type': forms.Select(attrs={'class': 'form-select'}),
-                'category': forms.Select(attrs={'class': 'form-select'}),
-            }
-
-        def __init__(self, *args, **kwargs):
-            super(TransactionForm, self).__init__(*args, **kwargs)
-            self.fields['date'].initial = datetime.now().date()
-
-
-class TransactionUpdateForm(forms.ModelForm):
     class Meta:
         model = Transaction
-        fields = ['date', 'description', 'amount', 'amount_type', 'category']
+        fields = ['display_date', 'description', 'amount', 'amount_type', 'category']
         widgets = {
-            'date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'display_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
             'description': forms.Textarea(
                 attrs={'class': 'form-control', 'placeholder': 'Please enter your description', 'rows': 3}),
             'amount': forms.NumberInput(
@@ -49,3 +27,29 @@ class TransactionUpdateForm(forms.ModelForm):
             'amount_type': forms.Select(attrs={'class': 'form-select'}),
             'category': forms.Select(attrs={'class': 'form-select'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super(TransactionForm, self).__init__(*args, **kwargs)
+        self.fields['display_date'].initial = datetime.now().date()
+
+
+class TransactionUpdateForm(forms.ModelForm):
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), required=False)
+    display_date = forms.DateField(initial=datetime.now().date(), widget=forms.DateInput(attrs={'type': 'date'}))
+
+    class Meta:
+        model = Transaction
+        fields = ['display_date', 'description', 'amount', 'amount_type', 'category']
+        widgets = {
+            'display_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'description': forms.Textarea(
+                attrs={'class': 'form-control', 'placeholder': 'Please enter your description', 'rows': 3}),
+            'amount': forms.NumberInput(
+                attrs={'class': 'form-control', 'placeholder': 'Please enter the amount of money you want'}),
+            'amount_type': forms.Select(attrs={'class': 'form-select'}),
+            'category': forms.Select(attrs={'class': 'form-select'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(TransactionUpdateForm, self).__init__(*args, **kwargs)
+        self.fields['display_date'].initial = datetime.now().date()
